c16134bed9dd32447de80a08e96452868b174d8d
hleinone <hleinone@19b530d0-94d4-11de-adcb-5b537d459081>
2009-09-18 02:46:27 +0000

    Organized imports.

--- spring-validation-annotations/trunk/src/test/java/org/beardedgeeks/spring/validation/annotations/ExtendedValidationUtilsTest.java
+++ spring-validation-annotations/trunk/src/test/java/org/beardedgeeks/spring/validation/annotations/ExtendedValidationUtilsTest.java
@@ -1,10 +1,10 @@
 package org.beardedgeeks.spring.validation.annotations;
 
-import static org.junit.Assert.assertEquals;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
 
 import java.lang.reflect.InvocationTargetException;
 
